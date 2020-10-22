909f81c8a1b6024e3200ec565737f93278a8ba0d
Prudhvieee <biyyappu.prudhvi@gmail.com>
2020-10-22 20:48:04 +0530

    [Prudhvi] Add . Adding uc2

--- Program.cs
+++ Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
-
-namespace EmployeeWageCalculation
+namespace EmployeeWageComputation
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to employee wage calculation");
+            const int IS_PRESENT_FULL_TIME = 1;
+            int empHrs = 0;
+            Random random = new Random();
+            ///Here, if loop is used to check employee wage.
+            ///if he is working for full time excutes the if condition 
+            ///otherwise excutes else part.
+            if (random.Next(0, 2) == IS_PRESENT_FULL_TIME)
+            {
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+            Console.WriteLine("Employee wage is " + 20 * empHrs);
         }
     }
 }
