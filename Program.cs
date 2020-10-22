1acfed4619f307acfe2277696363c8a18698d06d
Prudhvieee <biyyappu.prudhvi@gmail.com>
2020-10-22 21:08:02 +0530

    [Prudhvi] Add . Adding UC-4

--- Program.cs
+++ Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
-
-namespace EmployeeWageCalculation
+namespace EmployeeWageComputation
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to employee wage calculation");
+            const int IS_PRESENT_PART_TIME = 1;
+            const int IS_PRESENT_FULL_TIME = 2;
+            const int IS_ABSENT = 0;
+            int empHrs = 0;
+            ///Here, switch case statement is used to check employee wage.
+            ///if he present then it executes the switch case 1
+            ///if he absent then it executes the switch case 2
+            Random random = new Random();
+            switch (random.Next(0, 3))
+            {
+                case IS_PRESENT_PART_TIME:
+                    Console.WriteLine("Employee is present for part time");
+                    empHrs = 4;
+                    break;
+                case IS_PRESENT_FULL_TIME:
+                    Console.WriteLine("Employee is present for Full time");
+                    empHrs = 8;
+                    break;
+                case IS_ABSENT:
+                    Console.WriteLine("Employee is Absent");
+                    empHrs = 0;
+                    break;
+            }
+            Console.WriteLine("Employee wage is " + 20 * empHrs);
         }
     }
 }
