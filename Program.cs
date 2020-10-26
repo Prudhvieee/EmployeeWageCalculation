d1bd7381be19aec07fbd9f523b4aff2fd0226f19
Prudhvieee <biyyappu.prudhvi@gmail.com>
2020-10-26 07:42:59 +0530

    [Prudhvi] Add . Adding UC-9

--- Program.cs
+++ Program.cs
@@ -5,21 +5,12 @@ namespace EmployeeWageComputation
     {
         static void Main(string[] args)
         {
-            const int IS_PRESENT_FULL_TIME = 1;
-            int empHrs = 0;
-            Random random = new Random();
-            ///Here, if loop is used to check employee wage.
-            ///if he is working for full time excutes the if condition 
-            ///otherwise excutes else part.
-            if (random.Next(0, 2) == IS_PRESENT_FULL_TIME)
-            {
-                empHrs = 8;
-            }
-            else
-            {
-                empHrs = 0;
-            }
-            Console.WriteLine("Employee wage is " + 20 * empHrs);
+            EmployeeWageBuilder apple = new EmployeeWageBuilder("Apple", 50, 20, 100);
+            EmployeeWageBuilder google = new EmployeeWageBuilder("Google", 60, 20, 110);
+            apple.ComputeEmployeeWage();
+            Console.WriteLine(apple.toString());
+            google.ComputeEmployeeWage();
+            Console.WriteLine(google.toString());
         }
     }
 }
