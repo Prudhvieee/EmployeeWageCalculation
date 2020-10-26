b257fa52d6d85098283b7f703b80547058e85333
Prudhvieee <biyyappu.prudhvi@gmail.com>
2020-10-26 08:13:04 +0530

    [Prudhvi] Add . Adding UC-10 code

--- Program.cs
+++ Program.cs
@@ -1,25 +1,15 @@
-﻿using System;
+﻿using EmployeeWageCalculation;
+using System;
 namespace EmployeeWageComputation
 {
     class Program
     {
         static void Main(string[] args)
         {
-            const int IS_PRESENT_FULL_TIME = 1;
-            int empHrs = 0;
-            Random random = new Random();
-            ///Here, if loop is used to check employee wage.
-            ///if he is working for full time excutes the if condition 
-            ///otherwise excutes else part.
-            if (random.Next(0, 2) == IS_PRESENT_FULL_TIME)
-            {
-                empHrs = 8;
-            }
-            else
-            {
-                empHrs = 0;
-            }
-            Console.WriteLine("Employee wage is " + 20 * empHrs);
+            EmployeeBuilderArray employeeBuilderArray = new EmployeeBuilderArray();
+            employeeBuilderArray.AddCompanyEmpWage("IBM", 50, 20, 100);
+            employeeBuilderArray.AddCompanyEmpWage("HM", 60, 20, 110);
+            employeeBuilderArray.ComputeEmployeeWage();
         }
     }
 }
